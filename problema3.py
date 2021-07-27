042213b3a90a89379eb36eada31ab8e4314e58a2
Brandon <brandonhernandez_10@outlook.es>
2021-07-27 13:17:28 -0600

    Parte 3c y parte del 3d

--- problema3.py
+++ problema3.py
@@ -1,10 +1,49 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+bigrams = '00 01 10 11'
+trigrams = '000 001 010 011 100 101 110 111'
+
+blist = bigrams.split(' ')
+tlist = trigrams.split(' ')
+
+
 def logicalXor(bits1, bits2):
     res = ""
 
     # Se empieza por el ultimo bit
     for i in range(len(bits1)-1, -1, -1):
         # Se opera xor
-        res += str(int(bits1[i])^int(bits2[i]))
+        res += str(int(bits1[i]) ^ int(bits2[i]))
     return res
 
+
+def countGram(elements, chain):
+    counted = {}
+    n = len(elements[0])
+    # Se separan los elementos
+    separated = [chain[i:i+n] for i in range(0, len(chain), n)]
+    for i in elements:
+        counted[i] = separated.count(i)
+    return counted
+
+
+def makeHistogram(count):
+    values = list(count.values())
+    xtitle = list(count.keys())
+
+    plt.bar(xtitle, values)
+    plt.show()
+
+
+def generateY(length):
+    y = ''
+    numbers = np.random.choice(2, length)
+    for i in numbers:
+        y += str(i)
+    return y
+
 print(logicalXor("00000101", "00000011"))
+# generateY(6)
+# count = countGram(blist, '00000101')
+# makeHistogram(count)
