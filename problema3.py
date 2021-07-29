7159286a6622465ef845f49eae63871a37528115
Brandon <brandonhernandez_10@outlook.es>
2021-07-28 18:31:02 -0600

    Bugfix2

--- problema3.py
+++ problema3.py
@@ -12,7 +12,7 @@ def logicalXor(bits1, bits2):
     res = ""
 
     # Se empieza por el ultimo bit
-    for i in range(len(bits1)-1):
+    for i in range(len(bits1)):
         # Se opera xor
         res += str(int(bits1[i]) ^ int(bits2[i]))
     return res
